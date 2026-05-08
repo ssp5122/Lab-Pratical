e1184b60ae8ee732d0fbad44ce4bca4fac717b49
Shresth <ssp5122005@gmail.com>
2026-05-08 11:25:36 +0530

    Initial commit
    # Conflicts:
    #	const.jsx

---
+++ const.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Input() {
+  const [name, setName] = useState("Enter name");
+
+  return (
+    <div>
+      <h2>{name}</h2>
+
+      <input 
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default Input;
